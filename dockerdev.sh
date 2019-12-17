0a435ae9ae4648b35f94c88d4467098c4f9d67a2
NigelThomas <nigel.cl.thomas@gmail.com>
2019-12-17 10:18:09 +0000

    complete the basic slab load mechanism

--- dockerdev.sh
+++ dockerdev.sh
@@ -35,8 +35,34 @@ do
     if [ -e $f ]
     then
         Echo posting $f
-        curl -d@$f http://localhost/_project_import/user
+
+        # TODO check if project already exists, fail if so
+
+        (
+             # We need to insert adjustments into the slab file just before the final }
+             # these elements include their quotes
+ 
+             title=$(cat $f | jq .projectModel.title)
+             schema=$(cat $f | jq .projectModel.projectSchema)
+             
+             # and we have to wrap the whole thing with the outer elements here
+         
+             cat $f | sed -e 's/}$//' ; echo ", \"adjustments\": {\"newName\":\"$(basename $f .slab)\",\"newTitle\":$title,\"newSchema\":$schema, \"setProtect\": \"false\"}}" 
+        ) > /tmp/$f.1
+
+        # now stringify the slab file
+        cat /tmp/$f.1 | jq '. | tostring' > /tmp/$f.2
+
+        # and wrap it up for the call into a single line
+        (echo '{"username": "user", "json":' ; cat /tmp/$f.2 ; echo '}' ) | sed -e 's/\n//'  > /tmp/$f.3 
+        
+        # call the API
+        curl -H "Content-Type: application/json" -d@/tmp/$f.3 http://localhost:5585/_project_import/user
+
+        # TODO check response
+
+        rm /tmp/$f.{1,2,3}
     else
-        echo No such file $f
+        echo No such project export file $f
     fi
 done
