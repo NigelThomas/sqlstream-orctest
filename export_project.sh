6cef0eac9b08c917020b6272a04dcb450027252c
NigelThomas <nigel.cl.thomas@gmail.com>
2020-01-05 14:22:17 +0000

    remove files which have been absorbed into sqlstream-docker-utils

--- export_project.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Export the orctest project from StreamLab
-
-curl http://localhost:5585/_project_export/user/orctest
-curl -o orctest.slab http://localhost:5585/_download/user/orctest
