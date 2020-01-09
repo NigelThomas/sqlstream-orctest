88aaa5a71af39be7b059197033066fd5399ab724
NigelThomas <nigel.cl.thomas@gmail.com>
2020-01-09 18:21:13 +0000

    make sure still owned by sqlstream

--- pre-startup.sh
+++ pre-startup.sh
@@ -5,7 +5,8 @@
 # Assume we are running in the project directory
 
 
-cp -v hadoop/core-site.xml hadoop/hdfs-site.xml hadoop/svc*.keytab ..
+su sqlstream -m -c "cp -v hadoop/core-site.xml hadoop/hdfs-site.xml hadoop/svc*.keytab .."
+
 ls -l
 
 echo ... add definitions to hosts file
