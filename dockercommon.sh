43a5cd4b6638d350916d67024da7562528963ac6
NigelThomas <nigel.cl.thomas@gmail.com>
2020-01-16 14:50:28 +0000

    Revert adding of props files

--- dockercommon.sh
+++ dockercommon.sh
@@ -26,12 +26,12 @@ fi
 # mount the custom JNDI directory if needed (else we use the git repo's jndi directory
 # note: you may use the project's own jndi directory in which case working copies of properties files will override committed/pushed copies
 
-#if [ -n "$HOST_JNDI_DIR" ]
-#then
-#    HOST_JNDI_MOUNT="-v ${HOST_JNDI_DIR:=$HERE/jndi}:$CONTAINER_JNDI_DIR"
-#fi
+if [ -n "$HOST_JNDI_DIR" ]
+then
+    HOST_JNDI_MOUNT="-v ${HOST_JNDI_DIR:=$HERE/jndi}:$CONTAINER_JNDI_DIR"
+fi
 
-docker run -v ${HOST_DATA_SOURCE:=$HOME/vzw/iot}:$CONTAINER_DATA_SOURCE $HOST_TGT_MOUNT \
+docker run -v ${HOST_DATA_SOURCE:=$HOME/vzw/iot}:$CONTAINER_DATA_SOURCE $HOST_TGT_MOUNT $HOST_JNDI_MOUNT \
            -p 80:80 -p 5560:5560 -p 5580:5580 -p 5585:5585 -p 5590:5590 \
            -e GIT_ACCOUNT=$GIT_ACCOUNT -e GIT_PROJECT_NAME=$GIT_PROJECT_NAME \
            -e LOAD_SLAB_FILES="${LOAD_SLAB_FILES:=CSVingest.slab WriteORCtoHDFS.slab}" \
