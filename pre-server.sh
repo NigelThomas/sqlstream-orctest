f579272a1ae83e703a516e8941b66ff07f2e2b8a
NigelThomas <nigel.cl.thomas@gmail.com>
2020-02-07 15:34:17 +0000

    split out pre-server.sh from pre-startup.sh

---
+++ pre-server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Do any preproject setup needed before starting s-Server
+# Assume we are running in the project directory
+
+echo ... set up hadoop configuration
+# copied to /home/sqlstream because longer paths seem to cause a problem
+
+su sqlstream -m -c "cp -v hadoop/core-site.xml hadoop/hdfs-site.xml hadoop/svc*.keytab .."
+
+#echo ... add definitions to hosts file - doesn't seem to be needed
+#cat hadoop/testhosts >> /etc/hosts
+
+if [ -e hadoop/krb5.conf ]
+then
+    echo ... installing krb5.conf
+    cp hadoop/krb5.conf /etc
+fi
+# 
+
--- pre-startup.sh
+++ pre-startup.sh
@@ -4,15 +4,6 @@
 #
 # Assume we are running in the project directory
 
-
-su sqlstream -m -c "cp -v hadoop/core-site.xml hadoop/hdfs-site.xml hadoop/svc*.keytab .."
-
-ls -l
-
-echo ... add definitions to hosts file
-
-#cat hadoop/testhosts >> /etc/hosts
-
 echo ... Creating the clean_edrs interface stream
 
 sqllineClient --run=$(which clean_edrs.sql)
